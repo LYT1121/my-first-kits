4fdf9b7146c0ac6097fd3e8bd2a2a69b9b5034c0
李小姐 <1121024033@qq.com>
2019-07-20 21:21:17 +0800

    自动切换tab栏方法

--- tab.js
+++ tab.js
@@ -50,3 +50,84 @@ class Tab {
         currentContent.classList.add(this.contentClass);
     }
 }
+/**
+ * @author cwq //1121024033@qq.com
+ * @date 2019/7/20
+ * @description 封装的一个自动切换tab栏方法
+ */
+//创建一个自定义构造函数并使用extends继承上一个tab函数的属性和方法
+class autoTab extends Tab {
+    constructor(options) {
+      //在autoTab构造函数的最前面调用Tab
+      super(options);
+      options = options || {};
+      //新增动态的定时器时间属性和tabli个数
+      this.time = options.time || 1000;
+      this.num = options.num || 2;
+    }
+    autoPaly() {
+      //定义一个变量表示当前tab是第几个
+      let index = 0;
+      //获取定时器id
+      let intervalId = setInterval(() => {
+        //获取tab栏li元素和对应单块内容元素
+        let lis = document.querySelectorAll(this.navSelector);
+        let contents = document.querySelectorAll(this.contentSelector);
+        //当前tab为最后一个时将index改为0
+        index == this.num ? index = 0 : index++;
+        //调用tab排他加特殊样式方法
+        this.changeNavStyle(lis, lis[index])
+        //调用对应内容块排他加特殊样式方法
+        this.changeContentStyle(contents, contents[index])
+  
+      }, this.time);
+      //调用停止自动切换方法将定时器id传入方法
+      this.stopPaly(intervalId);
+    }
+    //tab排他加特殊样式方法
+    changeNavStyle(lis, currentLi) {
+      //遍历伪数组将所有tab样式变一致
+      lis.forEach(e => {
+        e.classList.remove(this.navClass);
+      })
+      //
+      currentLi.classList.add(this.navClass);
+    }
+    // 修改内容的样式的方法
+    changeContentStyle(contents, currentContent) {
+      contents.forEach(e => {
+        e.classList.remove(this.contentClass);
+      })
+      // 把对应div显示
+      currentContent.classList.add(this.contentClass);
+    }
+    //停止自动切换方法
+    stopPaly(Id) {
+      //闭包存储实例对象的this
+      let _this = this;
+      //获取tab栏li元素和对应单块内容元素
+      let lis = document.querySelectorAll(this.navSelector);
+      let contents = document.querySelectorAll(this.contentSelector);
+      //遍历tab栏元素
+      lis.forEach((e, i) => {
+        //设置所有tab的自定义id
+        e.setAttribute('data-id', i);
+        //给所有tab绑定事件
+        e.addEventListener(this.type, function () {
+          //清除停止定时器
+          clearInterval(Id);
+          //获取当前触发的tab存储
+          let eq = this.getAttribute('data-id');
+          //调用tab排他加特殊样式方法并将当前tab索引传参，改变当前对应的tab
+          _this.changeNavStyle(lis, lis[eq]);
+          //调用修改内容的样式的方法并将当前tab索引传参，改变当前对应的内容
+          _this.changeContentStyle(contents, contents[eq]);
+        });
+        //给所有tab注册鼠标离开事件
+        e.addEventListener('mouseout', function () {
+          //重新调用自动切换方法
+          _this.autoPaly();
+        });
+      });
+    }
+  }
